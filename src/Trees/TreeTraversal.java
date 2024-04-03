1ffc4f3416b09fdda3b3bf2ef440bf6e963bd7aa
dinukah99 <dinukahamarasinghe@gmail.com>
2024-04-03 18:36:40 +0530

    tree traversal methods

---
+++ src/Trees/TreeTraversal.java
@@ -0,0 +1,117 @@
+package Trees;
+
+public class TreeTraversal {
+
+    private class Node {
+        private int value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node=" + value;
+        }
+    }
+
+    private Node root;
+
+    public void insert(int value) {
+        Node node = new Node(value);
+        if (root == null) {
+            root = node;
+            return;
+        }
+
+        var current = root;
+        while(true) {
+            if(value < current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild =node;
+                    break;
+                }
+                current = current.leftChild;
+            }
+            else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                }
+                current = current.rightChild;
+
+            }
+        }
+    }
+
+
+    public boolean find(int value) {
+        var current = root;
+        while(current != null) {
+            if(value < current.value) {
+                current = current.leftChild;
+            } else if (value > current.value) {
+                current = current.rightChild;
+            } else
+                return true;
+        }
+        return false;
+    }
+
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        traverseInOrder(root.leftChild);
+        System.out.println(root.value);
+        traversePreOrder(root.rightChild);
+
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void traversePostOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.println(root.value);
+    }
+
+    public static void main(String[] args) {
+        TreeTraversal tree = new TreeTraversal();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+        tree.traversePreOrder();
+        tree.traverseInOrder();
+        tree.traversePostOrder();
+    }
+}
