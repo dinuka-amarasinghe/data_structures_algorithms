d0ccae41fe1766e5c2f269b173f915a5f56e8316
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-12 18:05:40 +0530

    Array:Find Largest Number

--- src/Arrays/FindLargestNumber.java
+++ src/Arrays/FindLargestNumber.java
@@ -3,16 +3,21 @@ package Arrays;
 public class FindLargestNumber {
 
     private static int findLargestNumber(int[] numbers) {
-        int largest = numbers[0];
-        for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] > largest)
-                largest = numbers[i];
+        if(numbers.length == 0)
+            throw new IllegalArgumentException("Array is empty");
+        else {
+            int largest = numbers[0];
+
+            for (int i = 0; i < numbers.length; i++) {
+                if(numbers[i] > largest)
+                    largest = numbers[i];
+            }
+            return largest;
         }
-        return largest;
     }
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] numbers = {};
         System.out.println(findLargestNumber(numbers));
     }
 }
