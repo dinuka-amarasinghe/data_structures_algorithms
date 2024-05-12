c30a30f1171d78aa80a70a8e7cdc3aeb2e22f1af
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-12 23:59:22 +0530

    String Checker even index

---
+++ src/Strings/EvenIndexCharacter.java
@@ -0,0 +1,26 @@
+package Strings;
+
+public class EvenIndexCharacter {
+
+    public static boolean isAtEvenIndex(String s, char item) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i += 2) {
+            if (s.charAt(i) == item) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "HeLLo";
+        System.out.println(isAtEvenIndex(s, 'L'));
+        System.out.println(isAtEvenIndex(s, 'T'));
+        System.out.println(isAtEvenIndex(s, 'H'));
+        System.out.println(isAtEvenIndex("", 'H'));
+        System.out.println(isAtEvenIndex(null, 'H'));
+    }
+}
