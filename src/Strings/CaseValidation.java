e85da7a4faac15f956605914bab045c4c354764b
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-12 21:06:04 +0530

    String:Validate Upper/Lowercase

---
+++ src/Strings/CaseValidation.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class CaseValidation {
+
+    public static boolean isUpperCase(String s) {
+        return s.chars().allMatch(Character::isUpperCase);
+    }
+
+    public static boolean isLowerCase(String s) {
+        return s.chars().allMatch(Character::isLowerCase);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUpperCase("hello"));
+        System.out.println(isUpperCase("HELLO"));
+
+        System.out.println(isLowerCase("hello"));
+        System.out.println(isLowerCase("HELLO"));
+    }
+}
