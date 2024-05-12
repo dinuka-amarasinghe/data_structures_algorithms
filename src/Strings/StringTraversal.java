f3a581bc392b673b75952e97b7b904429fc49dfc
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-12 23:50:13 +0530

    String: string character iteration methods

---
+++ src/Strings/StringTraversal.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class StringTraversal {
+
+    public static void parseContents(String s) {
+        System.out.println("Option 1");
+        for(char c : s.toCharArray()) {
+            System.out.println(c);
+        }
+        System.out.println();
+        System.out.println("Option 2");
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            System.out.println(c);
+        }
+    }
+    public static void main(String[] args) {
+        String s = "HeLLo";
+        parseContents(s);
+    }
+}
