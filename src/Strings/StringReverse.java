eb48cf655e5d433d4cb23fbb8c11cdfa7ab2250f
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-13 00:27:44 +0530

    String reversal using built in function

--- src/Strings/StringReverse.java
+++ src/Strings/StringReverse.java
@@ -1,10 +1,12 @@
 package Strings;
 
+import java.sql.SQLOutput;
+
 public class StringReverse {
 
     public static String reverse(String s) {
         if (s == null || s.isEmpty()) {
-            return "";
+            return "-";
         }
 
         StringBuilder reversed = new StringBuilder();
@@ -14,11 +16,26 @@ public class StringReverse {
         return reversed.toString();
     }
 
+    public static String reverse2(String s) {
+        if (s == null || s.isEmpty()) {
+            return "-";
+        }
+        StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString();
+    }
+
     public static void main(String[] args) {
+        System.out.println("Reverse 1");
         System.out.println(reverse(null));
         System.out.println(reverse(""));
         System.out.println(reverse("hello"));
         System.out.println(reverse("tacos"));
         System.out.println(reverse("Hi!"));
+        System.out.println("Reverse 2");
+        System.out.println(reverse2(null));
+        System.out.println(reverse2(""));
+        System.out.println(reverse2("hello"));
+        System.out.println(reverse2("tacos"));
+        System.out.println(reverse2("Hi!"));
     }
 }
