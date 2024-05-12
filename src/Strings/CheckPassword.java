06e3571f5aac48b90892b1d8a5d30b806d2fa26b
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-12 21:11:15 +0530

    String: Validation Algorithm -> Check Password Complexity

---
+++ src/Strings/CheckPassword.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class CheckPassword {
+    public static boolean isPasswordComplex(String s) {
+        return s.chars().anyMatch(Character::isUpperCase) &&
+        s.chars().anyMatch(Character::isLowerCase) &&
+        s.chars().anyMatch(Character::isDigit);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPasswordComplex("Hell0"));
+        System.out.println(isPasswordComplex("Hello"));
+        System.out.println(isPasswordComplex("hell0"));
+        System.out.println(isPasswordComplex("HeLLo"));
+        System.out.println(isPasswordComplex("hello"));
+        System.out.println(isPasswordComplex(" "));
+    }
+}
