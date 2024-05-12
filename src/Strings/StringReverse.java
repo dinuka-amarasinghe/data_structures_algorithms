2145924024ea2cad3c354567c147678fa699a9b1
dinukah99 <dinukahamarasinghe@gmail.com>
2024-05-13 00:08:49 +0530

    String: String reversal

---
+++ src/Strings/StringReverse.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class StringReverse {
+
+    public static String reverse(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder reversed = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed.append(s.charAt(i));
+        }
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(null));
+        System.out.println(reverse(""));
+        System.out.println(reverse("hello"));
+        System.out.println(reverse("tacos"));
+        System.out.println(reverse("Hi!"));
+    }
+}
